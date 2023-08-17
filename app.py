bac526c3142ffe62189c4e20c59a4ac9238205d6
SHANMUGARAJA K <shanmugaraj.kumar@gmail.com>
2023-08-17 17:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 #print(app.config)
 
-@app.route("/home")
+@app.route("/api")
 def home():
 	return "Flask API get endpoint running"
+
+@app.route("/api/getUsers")
+def getUsers():
+	return jsonify({"first_name":"shanmuga", "last_name":"raja", "email":"shanmugaraj.kumar@gmail.com"})
 
 if __name__ == "__main__":
 	app.run(debug=True)
